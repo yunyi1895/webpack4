a0d3e1a6513dd9ea60244e59e204ee2dffc1ef00
MAC <mac@MACdeMacBook-Pro.local>
2019-05-13 13:17:52 +0800

    配置一完成

--- demo1/webpack.config.js
+++ demo1/webpack.config.js
@@ -8,5 +8,12 @@ module.exports={ // commonJs 语法
     path:path.resolve(__dirname,'dist'),  // 输出的文件地址，是绝对路径。没有则创建新的文件夹。
     filename:'[name].js',  // 输出js的名字。[name]是对应的入口的文件名。
     publicPath:'./assets' // 是相对路径，相对于path的路径。储层静态资源的路径，如css、图片、字体等。
+    // publicPath: "https://cdn.example.com/assets/", // CDN（总是 HTTPS 协议）
+    // publicPath: "//cdn.example.com/assets/", // CDN (协议相同)
+    // publicPath: "/assets/", // 相对于服务(server-relative)
+    // publicPath: "assets/", // 相对于 HTML 页面
+    // publicPath: "../assets/", // 相对于 HTML 页面
+    // publicPath: "", // 相对于 HTML 页面（目录相同）
+    // publicPath 一般配置为相对于服务
   }
 }
